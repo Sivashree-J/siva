672436a8081278cc66f069e2f7fd4c848a6049a8
Sivashree-J <139237515+Sivashree-J@users.noreply.github.com>
2023-07-21 15:48:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return 'etho aaru'
+    return 'etho1'
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
